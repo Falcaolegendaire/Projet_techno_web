1b4615bb9f79e7b5a8db910f5c0248d465707107
Bryan-programming <tujilojoyce@gmail.com>
2025-03-28 14:32:54 +0100

    ajout des fichier css et creation des pages html des menu de la taskbar

--- Script/script_acceuil.js
+++ Script/script_acceuil.js
@@ -1,4 +1,56 @@
 let boutons = document.getElementsByClassName("button");
+let bouton_offre=document.getElementById("button_offre");
+let bouton_ajout_offre=document.getElementById("button_ajouter_offre");
+let bouton_suggestion=document.getElementById("button_suggestion");
+let bouton_notification=document.getElementById("button_notification");
+let bouton_contact=document.getElementById("button_contact");
+let bouton_aide=document.getElementById("button_aide");
+let bouton_profil=document.getElementById("button_profil");
+let bouton_logout=document.getElementById("button_log_out");
+
+function redirect_to_another_page(path,target) {
+    window.open(path, target);
+}
+// debut des instructions pour pouvoir rediger les users quand ils vont cliquer sur les boutons
+bouton_offre.addEventListener('click',function(){
+        redirect_to_another_page("./offre.html","_parent");
+});
+
+bouton_ajout_offre.addEventListener('click',function(){
+    redirect_to_another_page("./ajouter_offre.html","_parent");
+});
+
+bouton_suggestion.addEventListener('click',function(){
+    redirect_to_another_page("./suggestions.html","_parent");
+});
+
+bouton_notification.addEventListener('click',function(){
+    redirect_to_another_page("./notification.html","_parent");
+});
+
+bouton_contact.addEventListener('click',function(){
+    redirect_to_another_page("./contact.html","_parent");
+});
+
+bouton_aide.addEventListener('click',function(){
+    redirect_to_another_page("./aide.html","_parent");
+});
+
+bouton_profil.addEventListener('click',function(){
+    redirect_to_another_page("./profil.html","_parent");
+});
+
+bouton_logout.addEventListener('click',function(){
+    let bool;
+    bool=confirm(" \u26A0\uFE0F voulez vous vraiment vous deconnectez?");
+    if (bool){
+        alert("deconnexion reussie 	\u2705");
+        window.location.replace("./connexion.html"); 
+    }
+    
+});
+//  fin 
+
 for (let i = 0; i < boutons.length; i++) {
     boutons[i].addEventListener("mouseover", function() {
         boutons[i].style.backgroundColor = "rgb(186, 218, 213)"; 
@@ -7,12 +59,4 @@ for (let i = 0; i < boutons.length; i++) {
     boutons[i].addEventListener("mouseout", function() {
         boutons[i].style.backgroundColor = ""; 
     });
-
-function logout(){
-    let bool;
-    bool=confirm("voulez vous vraiment vous deconnectez?");
-    if(bool){
-        window.open("./connexion.html", "_parent"); // par defaut le script est consideré comme etant dans le dossier du fichier html dont il est relié
-        }
-    } 
 }
